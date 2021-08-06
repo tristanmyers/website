0643b4c74812aa36d634423aae423f89169db4bb
tristanmyers <tristanmyers.developer@gmail.com>
2021-08-06 15:39:24 -0400

    created ProjectCard custom element

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,31 @@
-// maybe i add some js at some point in time
+// project card custom element
+class ProjectCard extends HTMLElement {
+    constructor() {
+        super();
+        
+        const shadow = this.attachShadow({mode: 'open'});
 
+        const projectCardContainer = document.createElement('section');
+        projectCardContainer.setAttribute('class', 'project-card-container');
+        projectCardContainer.setAttribute('part', 'card-container')
+
+        const helloWorld = document.createElement('p');
+        helloWorld.innerHTML = 'Hello World';
+        // styling
+        const style = document.createElement('style');
+        style.textContent = ".project-card-container{width: 100px;height: 100px;border: 2px solid black;}"
+
+        projectCardContainer.appendChild(helloWorld);
+        projectCardContainer.appendChild(style);
+
+        shadow.appendChild(projectCardContainer);
+    }
+
+}
+
+customElements.define('project-card', ProjectCard);
+
+const projectsContainer = document.querySelector('#projects-container');
+const projectCard = document.createElement('project-card');
+
+projectsContainer.appendChild(projectCard);
