ec5f6472adbf1a5290428ab4adfa0572b896bb9c
tristanmyers <tristanmyers.developer@gmail.com>
2021-08-10 16:24:20 -0400

    removed projectcard class

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,99 +1,15 @@
-// project card custom element
-class ProjectCard extends HTMLElement {
-    constructor() {
-        super();
-                
-        const shadow = this.attachShadow({mode: 'open'});
+import ProjectCard from './ProjectCard.js'
 
-        const projectCardContainer = document.createElement('section');
-        projectCardContainer.setAttribute('class', 'project-card-container');
-        
-        // card styling     
-        shadow.innerHTML = 
-        `
-        <style>
-        .project-card-container {
-            background-color: #292E33;
-            font-family: Noto Sans JP;
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-            padding-bottom: 10px;
-        }
-
-        .project-image-container {
-            height: 60%;
-            width: 100%;
-            padding: 5px 0px 10px 0px;
-        }
-
-        .project-image-container img{
-            height: 100%;
-            width: 100%;
-        }
-
-        a, a:visited{
-            color: white;
-        }
-
-        #project-name {
-            text-align: center;
-        }
-
-        #project-desc {
-            padding-left: 5px;
-        }
-
-        #project-tags {
-            padding-left: 5px;
-        }
-        </style>
-        `
-        
-        // card content
-        // will make this more dynamic for github api later
-        // could turn this into an object
-        const projectImageContainer = document.createElement('div');
-        projectImageContainer.setAttribute('class', 'project-image-container');
-
-        const projectImage = document.createElement('img');
-        projectImage.setAttribute('src', 'https://images.pexels.com/photos/4451844/pexels-photo-4451844.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-        projectImage.setAttribute('alt', 'thumbnail for project');
-        projectImageContainer.appendChild(projectImage)
-        
-        const projectName = document.createElement('a');
-        projectName.setAttribute('id', 'project-name');
-        projectName.setAttribute('href', 'https://nifty-heyrovsky-83f270.netlify.app/');
-        projectName.innerHTML = 'Slapjack Card Game';
-
-        const projectDesc = document.createElement('p');
-        projectDesc.setAttribute('id', 'project-desc');
-        projectDesc.innerHTML = `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Lorem ipsum dolor sit amet.`;
-
-        const projectTags = document.createElement('p');
-        projectTags.setAttribute('id', 'project-tags');
-        projectTags.innerHTML = 'languages: js, html, scss/css';
-
-
-        projectCardContainer.appendChild(projectImageContainer);
-        projectCardContainer.appendChild(projectName);
-        projectCardContainer.appendChild(projectDesc);
-        projectCardContainer.appendChild(projectTags);
-
-        shadow.appendChild(projectCardContainer);
-        
-    }
-
-}
+let repos = [];
 
 customElements.define('project-card', ProjectCard);
 
 const projectsContainer = document.querySelector('#projects-container');
 
-// could make this into a projectcard method and make constructor variables link to the response
-let repos = [];
+/* 
+at document load, fetch the repos from github and and push the repoinfo objects to repos array     
+then display each card on the site.
+*/
 async function getRepos() {
 
     try {
@@ -105,7 +21,7 @@ async function getRepos() {
         });
         let data = await response.json();
 
-        for (repo in data) {
+        for (let repo in data) {
             let repoInfo = {
                 'repo_name': data[repo].repo_name,
                 'repo_desc': data[repo].repo_desc,
@@ -114,15 +30,15 @@ async function getRepos() {
 
            repos.push(repoInfo)
         }
-        
-        
-        
     } catch(err) {
         console.error(err);
     }
 
-    for (repo in repos) {
+    for (let repo in repos) {
         const projectCard = document.createElement('project-card');
+        projectCard.setAttribute('name', repos[repo].repo_name);
+        projectCard.setAttribute('desc', repos[repo].repo_desc);
+        projectCard.setAttribute('url', repos[repo].repo_url);
         projectsContainer.appendChild(projectCard);
     }
 }
